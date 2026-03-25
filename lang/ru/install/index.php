45b2c3e8e50f70d9094824b25c0b7ba4a21cf064
Oleg Zabaluev <oziside@gmail.com>
2026-03-25 20:47:15 +0300

    feat(module): add Bitrix module install scaffolding

---
+++ lang/ru/install/index.php
@@ -0,0 +1,5 @@
+<?php
+$MESS['MODULE_NAME']        = 'Bitrix API Sample';
+$MESS['MODULE_DESCRIPTION'] = '';
+$MESS['PARTNER_NAME']       = 'Олег Забалуев';
+$MESS['PARTNER_URI']        = 'https://ozabaluev.ru/';

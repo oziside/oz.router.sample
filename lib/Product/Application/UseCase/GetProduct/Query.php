2a7cafa7648084d41f030113fc0c25665ca93ba8
Oleg Zabaluev <oziside@gmail.com>
2026-03-28 09:54:42 +0300

    feat(product): add application dto and mapper

---
+++ lib/Product/Application/UseCase/GetProduct/Query.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+namespace App\Product\Application\UseCase\GetProduct;
+
+
+final class Query
+{
+    public function __construct(
+        public readonly int $id
+    ) {}
+}
